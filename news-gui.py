c50d4352600febe19f50e1a6088b6b34a942abfe
Nic <stuff@wisefool.org>
2023-01-26 01:11:43 +0000

    Cosmetic changes - colours, spacing, etc

--- news-gui.py
+++ news-gui.py
@@ -40,7 +40,7 @@ canvas.bind_all("<Button-5>", scrolldown)
 window.bind_all("<Up>", scrollup)
 canvas.bind_all("<Down>", scrolldown)
 
-canvas.grid(row=1, column=0,rowspan=140, padx=20)
+canvas.grid(row=1, column=0,rowspan=140, padx=(20,5))
 
 
 links = IntVar(window, 0)
@@ -137,11 +137,29 @@ story_results = Label(text="",
                      highlightthickness=0,
                       font=("Arial", 16))
 
+current_time = Label(text="",
+                      background="black",
+                      foreground="#ff6600",
+                     anchor=W,
+                     highlightthickness=0,
+                      font=("Arial", 16))
+
+published_time = Label(text="",
+                      background="black",
+                      foreground="#ff6600",
+                     anchor=W,
+                     highlightthickness=0,
+                      font=("Arial", 16))
+
+
+
+
+
+
+
 
 def open_link(url):
    webbrowser.open_new_tab(url)
-
-
 
 
 def get_news():
@@ -163,29 +181,48 @@ def get_news():
 
         dynamic_var = f"label{x}"
         vars()[dynamic_var] = \
-            canvas.create_text(0, i, text=f"{stories[n]['story']}",
-                               font=("Arial", 16),
-                               fill="#ff6600", anchor=NW, tags="textTag")
-        i += 27
+            canvas.create_text(0,
+                               i,
+                               text=f"{stories[n]['story']}",
+                               font=("Arial", 15),
+                               fill="#ff6600",
+                               anchor=NW,
+                               tags="textTag")
+        i += 40
         x+=1
-        if links.get() == 1:
-            dynamic_var = f"label{x}"
-            vars()[dynamic_var] = \
-                canvas.create_text(0, i, text=f"{stories[n]['link']}",
-                                   font=("Arial", 16),
-                                   fill="#56a5f5", activefill="yellow", anchor=NW, tags="textTag")
-
-            canvas.tag_bind(vars()[dynamic_var],"<Button-1>", lambda e, link=stories[n]['link']:
-            open_link(link))
-            i += 27
-            x+=1
         if summaries.get() == 1:
             dynamic_var = f"label{x}"
             vars()[dynamic_var] = \
-                canvas.create_text(0, i, text=f"{stories[n]['summary']}",
+                canvas.create_text(0,
+                                   i-20,
+                                   text=f"{stories[n]['summary']}",
+                                   font=("Arial", 12),
+                                       fill="#e03663",
+                                   anchor=NW,
+                                   tags="textTag",
+                                   width=1030)
+            if len(stories[n]["summary"]) > 142:
+                i+=20
+            i += 18
+            x+=1
+            print(len(stories[n]["summary"]))
+        if links.get() == 1:
+            dynamic_var = f"label{x}"
+            vars()[dynamic_var] = \
+                canvas.create_text(0,
+                                   i-23,
+                                   text=f"{stories[n]['link']}",
                                    font=("Arial", 16),
-                                   fill="#f55882", anchor=NW, tags="textTag")
-            i += 27
+                                   fill="#0262c4",
+                                   activefill= "#51f505" ,
+                                   anchor=NW,
+                                   tags="textTag")
+
+            canvas.tag_bind(vars()[dynamic_var],
+                            "<Button-1>",
+                            lambda e, link=stories[n]['link']:
+                            open_link(link))
+            i += 16
             x+=1
 
 
@@ -193,7 +230,14 @@ def get_news():
               "text_files/newsaccessed.txt", "w") as writer:
         writer.write(str(my_news.now))
 
-    story_results.config(text=f"{len(stories)} Stories Displayed")
+    story_results.config(
+        text=f"{len(stories)} Stories Displayed")
+    current_time.config(
+        text=f"Display Time:\n"
+             f"{my_news.now.strftime('%a %d %b %H:%M')}")
+    published_time.config(
+        text=f"Feed Published:\n"
+             f"{my_news.rss_published_string}")
 
     canvas.create_text(0, i, text="")
     canvas.configure(yscrollcommand=scroll_y.set)
@@ -215,14 +259,16 @@ get_stories = Button(text="Get Stories",
 
 
 spacer.grid(row=0, rowspan=49, column=3)
-links_button.grid(row=5, column=4, columnspan=8)
-summaries_button.grid(row=6, column=4, columnspan=8)
+links_button.grid(row=6, column=4, columnspan=8)
+summaries_button.grid(row=5, column=4, columnspan=8)
 today.grid(row=13, column=4, columnspan=8)
 not_today.grid(row=14, column=4, columnspan=8)
 any_time.grid(row=15, column=4, columnspan=8)
 only_new_button.grid(row=25, column=4, columnspan=8)
-get_stories.grid(row=40, column=4,columnspan=7)
-story_results.grid(row=50,column=3,columnspan=9)
+get_stories.grid(row=40, column=4,columnspan=8)
+story_results.grid(row=60,column=3,columnspan=9, padx=(12,0))
+current_time.grid(row=75,column=3,columnspan=9, padx=(12,0))
+published_time.grid(row=90,column=3,columnspan=9, padx=(12,0))
 
 
 
